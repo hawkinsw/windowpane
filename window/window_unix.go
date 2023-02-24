186389a55b841e66700a8cac138b01863ced5b24
Will Hawkins <whh8b@obs.cr>
2023-02-24 16:50:39 -0500

    Add support for Window size on Linux (protoype).

--- window/window_unix.go
+++ window/window_unix.go
@@ -5,12 +5,40 @@ package window
 import (
 	"fmt"
 	"net"
+
+	"golang.org/x/sys/unix"
 )
 
 func GetSenderWindowAvailable() bool {
-	return false
+	return true
 }
 
 func GetSenderWindow(basicConn net.Conn) (uint32, error) {
-	return 0, fmt.Errorf("GetSenderWindow not available on UNIX platforms.")
+	var err error = nil
+	var info *unix.TCPInfo = nil
+	if info, err = getTCPInfo(basicConn); err == nil {
+		return info.Snd_wnd, nil
+	}
+	return 0, err
+}
+
+/*
+ * Blatant self plagiarism from goresponsiveness.
+ */
+func getTCPInfo(basicConn net.Conn) (*unix.TCPInfo, error) {
+	tcpConn, ok := basicConn.(*net.TCPConn)
+	if !ok {
+		return nil, fmt.Errorf(
+			"OOPS: Could not get the TCP info for the connection (not a TCP connection)",
+		)
+	}
+	rawConn, err := tcpConn.SyscallConn()
+	if err != nil {
+		return nil, err
+	}
+	var info *unix.TCPInfo = nil
+	rawConn.Control(func(fd uintptr) {
+		info, err = unix.GetsockoptTCPInfo(int(fd), unix.SOL_TCP, unix.TCP_INFO)
+	})
+	return info, err
 }
